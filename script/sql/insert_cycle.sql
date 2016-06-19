f6aa18a7a6df0b1ddba834025e78164773493dda
mathieu.rolland14270@orange.fr <mathieu.rolland14270@orange.fr>
2016-06-19 17:09:10 +0200

    Fix mysql execution : log deleted

--- script/sql/insert_cycle.sql
+++ script/sql/insert_cycle.sql
@@ -2,5 +2,7 @@
 	Script used to create new cycle in table monitoring.cycle
 */
 
-insert into CYCLE (timestamp, timeend) values ( @start_date, @end_date );
+insert into monitoring.CYCLE (timestamp, timeend) values ( @start_date, @end_date );
+select * from monitoring.CYCLE;
+
 commit;
